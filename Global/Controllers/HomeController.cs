adc2cab9dcda9e62195802be989fc7af4b349ad6
Luisa Martins <Luisa.Andre.Martins@formandos.cinel.pt>
2021-01-05 16:38:32 +0000

    Init Project

---
+++ Global/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Global.Models;
+
+namespace Global.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Sobre()
+        {
+            ViewData["Message"] = "A descrição da sua empresa.";
+
+            return View();
+        }
+
+        public IActionResult Servicos()
+        {
+            ViewData["Message"] = "Os seus serviços.";
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
