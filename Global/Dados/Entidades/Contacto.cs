60db47da5dfb5fceed038e95bf09bf916e64976a
Luisa Martins <Luisa.Andre.Martins@formandos.cinel.pt>
2021-01-06 23:43:05 +0000

    CRUD de contactos

---
+++ Global/Dados/Entidades/Contacto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Global.Dados.Entidades
+{
+    public class Contacto
+    {
+        public int Id { get; set; }
+
+
+        public string Nome { get; set; }
+
+
+        public string Email { get; set; }
+
+
+        public string Mensagem { get; set; }
+
+    }
+}
